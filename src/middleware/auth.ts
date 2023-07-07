58bac0b0699e63ce50323a7c65bd310612b23c9c
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-07-07 16:53:02 +0000

    In process

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,31 @@
-import { Request, Response, Application } from "express";
+import { Request, Response, Application, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import bcrypt from "bcrypt";
 
+dotenv.config();
+const { TOKEN_SECRET } = process.env;
+
+export interface CustomRequest extends Request {
+    token: string | jwt.JwtPayload;
+};
+
+export const VerifyAuthToken = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+
+        if(!token) {
+            throw new Error();
+        }
+
+        const decoded = jwt.verify(token, String(TOKEN_SECRET));
+        (req as CustomRequest).token = decoded;
+
+        next();
+    } catch (err) {
+        res.status(401).send("Please authenticate");
+    }
+};
